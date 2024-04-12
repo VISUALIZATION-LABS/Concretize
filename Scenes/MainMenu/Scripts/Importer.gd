0925d5fd2df472b52439d7e1946d5cb16a8e505d
Felipe-Sena <lipesenacosta@gmail.com>
2024-04-12 15:46:38 -0300

    Started work on the OBJ importer

--- Scenes/MainMenu/Scripts/Importer.gd
+++ Scenes/MainMenu/Scripts/Importer.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var testing_scene = preload("res://Scenes/3D/InternalTests/loadTest.tscn").instantiate()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -19,6 +19,8 @@ func _on_button_button_up() -> void:
 	
 	file_dialog.access = FileDialog.ACCESS_FILESYSTEM
 	file_dialog.file_mode = FileDialog.FILE_MODE_OPEN_FILE
+	file_dialog.initial_position = Window.WINDOW_INITIAL_POSITION_CENTER_MAIN_WINDOW_SCREEN
+	file_dialog.size = Vector2(500, 300)
 	file_dialog.filters = PackedStringArray(["*.obj ; Wavefront"])
 	file_dialog.visible = true
 	
@@ -29,19 +31,61 @@ func _on_button_button_up() -> void:
 func importer(path: String):
 	match (path.rsplit(".", true, 0)[1].to_lower()):
 		"obj":
-			var model: PackedStringArray = FileAccess.open(path, FileAccess.READ).\
-			get_as_text().rsplit("\n")
+			# Inital parsing with regex
+			print("Init parse\n\nOBJ_FILE:\n")
+			var object = RegEx.new()
+			object.compile("o.*[A-Za-z]+[0-9]*\\.[0-9]+")
 			
+			var vertex = RegEx.new()
+			vertex.compile("v\\s.*[0-9]*\\.[0-9]+.*[0-9]*\\.[0-9]+.*[0-9]*\\.[0-9]+")
+			
+			
+			#var model: PackedStringArray = FileAccess.open(path, FileAccess.READ).\
+			#get_as_text().rsplit("\n")
+			
+			var model: String = FileAccess.open(path, FileAccess.READ).get_as_text()
 			# TODO: Convert the model into the variables below
 			
 			# References:
 			# https://en.wikipedia.org/wiki/Wavefront_.obj_file
-			
+			var parsed_model = {}
+			var objects: PackedStringArray
 			var vertices: PackedVector3Array
 			var normals: PackedVector3Array
 			var uv: PackedVector3Array
 			var faces: PackedVector3Array
 			
+			var finished: bool = false
+			
+			
+			# FIXME: This doesn't work properly (gets all our data but doesn't
+			#        match up with objects) 
+			
+			print("Objects")
+			for result in object.search_all(model):
+				objects.append(result.get_string())
+			print(objects)
+			
+			print("Vertices")
+			for result in vertex.search_all(model):
+				
+				# With this we'll have the vertices, then we just need to convert
+				# them to floats
+				
+				var parsed_vertices: PackedStringArray = result.get_string().rsplit(" ").slice(1)
+				
+				vertices.append(Vector3(
+					float(parsed_vertices[0]),
+					float(parsed_vertices[1]),
+					float(parsed_vertices[2])
+					))
+			print(vertices)
+			
+			# Test buld
+			
+			InternalModel.vertex_array = vertices
+			
+			get_tree().change_scene_to_file("res://Scenes/3D/InternalTests/loadTest.tscn")
 			
 		"gltf":
 			#TODO: Implement
