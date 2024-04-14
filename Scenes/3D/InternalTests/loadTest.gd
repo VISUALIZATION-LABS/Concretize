a8c10ae6b6a07c7ffd907274599e934014f0b117
Felipe-Sena <lipesenacosta@gmail.com>
2024-04-14 00:42:46 -0300

    Dabbled with OBJ parsing. Failed

--- Scenes/3D/InternalTests/loadTest.gd
+++ Scenes/3D/InternalTests/loadTest.gd
@@ -3,32 +3,36 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("This is: LoadTest -> Felipe Sena Costa and Visualization Labs")
+	#print("This is: LoadTest -> Felipe Sena Costa and Visualization Labs")
+	#
+	#print("\nInternalModelVertexes: ")
+	#print(InternalModel.vertex_array)
+##
+	## Initialize the ArrayMesh.
+	#var temp_array: PackedVector3Array
+	#var arr_mesh = ArrayMesh.new()
+	#var arrays = []
+	#arrays.resize(Mesh.ARRAY_MAX)
+##
+	## Create the Mesh.
+	#for i in range(0, arrays.size()):
+		#if ((i - 3) % 3 == 0):
+			#for j in range(0,3):
+				#temp_array.append(InternalModel.vertex_array[j])
+				#arrays[Mesh.ARRAY_VERTEX] = temp_array
+		#arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	#
+		#
+		#print()
+	#
+	#var m = MeshInstance3D.new()
+	#m.mesh = arr_mesh
+	#
+	#get_tree().root.add_child(m)
+	var m := MeshInstance3D.new()
+	m.mesh = ModelParse.generate_obj()
 	
-	print("\nInternalModelVertexes: ")
-	print(InternalModel.vertex_array)
-#
-	# Initialize the ArrayMesh.
-	var temp_array: PackedVector3Array
-	var arr_mesh = ArrayMesh.new()
-	var arrays = []
-	arrays.resize(Mesh.ARRAY_MAX)
-#
-	# Create the Mesh.
-	for i in range(0, arrays.size()):
-		if ((i - 3) % 3 == 0):
-			for j in range(0,3):
-				temp_array.append(InternalModel.vertex_array[j])
-				arrays[Mesh.ARRAY_VERTEX] = temp_array
-		arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
-	
-		
-		print()
-	
-	var m = MeshInstance3D.new()
-	m.mesh = arr_mesh
-	
-	get_tree().root.add_child(m)
+	get_tree().root.add_child.call_deferred(m)
 	pass
 
 
--- Scenes/MainMenu/Scripts/Importer.gd
+++ Scenes/MainMenu/Scripts/Importer.gd
@@ -26,65 +26,11 @@ func _on_button_button_up() -> void:
 	
 	$MarginContainer/Control.add_child(file_dialog)
 	file_dialog.file_selected.connect(importer)
-	
 
 func importer(path: String):
 	match (path.rsplit(".", true, 0)[1].to_lower()):
 		"obj":
-			# Inital parsing with regex
-			print("Init parse\n\nOBJ_FILE:\n")
-			var object = RegEx.new()
-			object.compile("o.*[A-Za-z]+[0-9]*\\.[0-9]+")
-			
-			var vertex = RegEx.new()
-			vertex.compile("v\\s.*[0-9]*\\.[0-9]+.*[0-9]*\\.[0-9]+.*[0-9]*\\.[0-9]+")
-			
-			
-			#var model: PackedStringArray = FileAccess.open(path, FileAccess.READ).\
-			#get_as_text().rsplit("\n")
-			
-			var model: String = FileAccess.open(path, FileAccess.READ).get_as_text()
-			# TODO: Convert the model into the variables below
-			
-			# References:
-			# https://en.wikipedia.org/wiki/Wavefront_.obj_file
-			var parsed_model = {}
-			var objects: PackedStringArray
-			var vertices: PackedVector3Array
-			var normals: PackedVector3Array
-			var uv: PackedVector3Array
-			var faces: PackedVector3Array
-			
-			var finished: bool = false
-			
-			
-			# FIXME: This doesn't work properly (gets all our data but doesn't
-			#        match up with objects) 
-			
-			print("Objects")
-			for result in object.search_all(model):
-				objects.append(result.get_string())
-			print(objects)
-			
-			print("Vertices")
-			for result in vertex.search_all(model):
-				
-				# With this we'll have the vertices, then we just need to convert
-				# them to floats
-				
-				var parsed_vertices: PackedStringArray = result.get_string().rsplit(" ").slice(1)
-				
-				vertices.append(Vector3(
-					float(parsed_vertices[0]),
-					float(parsed_vertices[1]),
-					float(parsed_vertices[2])
-					))
-			print(vertices)
-			
-			# Test buld
-			
-			InternalModel.vertex_array = vertices
-			
+			ModelParse.obj = FileAccess.get_file_as_string(path)
 			get_tree().change_scene_to_file("res://Scenes/3D/InternalTests/loadTest.tscn")
 			
 		"gltf":
