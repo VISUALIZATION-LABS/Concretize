ef9caa354dd4e8085a6668979cffa4c3d3a38309
Felipe-Sena <lipesenacosta@gmail.com>
2024-05-13 06:47:22 -0300

    Added scaling based on view distance and a couple of other things

--- Scenes/Gizmo/Move/Scripts/moveGizmo.gd
+++ Scenes/Gizmo/Move/Scripts/moveGizmo.gd
@@ -1,31 +1,49 @@
 extends Node3D
-#
-#
-#var current_control = controlTypes.OUTSIDE
-#
-#var outline_3d_material = load("res://Scenes/Gizmo/Shared/selected_outline.tres")
-#var outline_planar_material = load("res://Scenes/Gizmo/Shared/selected_outline_planar.tres")
-#
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-#
-#
-#func _process(_delta: float) -> void:
+
+@export var scaling_factor: float = 0.3
+
+enum gizmo_control_types {
+	X,
+	Y,
+	Z,
+	GENERAL,
+	XZ,
+	XY,
+	YZ,
+	OUTSIDE
+}
+
+
+var current_control =gizmo_control_types.OUTSIDE
+var outline_3d_material = load("res://Scenes/Gizmo/Shared/selected_outline.tres")
+var outline_planar_material = load("res://Scenes/Gizmo/Shared/selected_outline_planar.tres")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(_delta: float) -> void:
+	var camera_position = get_viewport().get_camera_3d().global_position.length()
+	var delta_position = camera_position - self.global_position.length()
+	
+	self.scale = Vector3(delta_position, delta_position, delta_position) * scaling_factor
+	
+	
 	#match current_control:
-		##controlTypes.OUTSIDE:
-			##print("Mouse is outside of all controls")
-		##controlTypes.X:
-			##print("Mouse is inside of X at " + str(get_viewport().get_mouse_position()))
-		#controlTypes.Y:
+		#gizmo_control_types.OUTSIDE:
+			#print("Mouse is outside of all controls")
+		#gizmo_control_types.X:
+			#print("Mouse is inside of X at " + str(get_viewport().get_mouse_position()))
+		#gizmo_control_types.Y:
 			#print("Mouse is inside of Y at " + str(get_viewport().get_mouse_position()))
-	#pass
-#
-#func _on_y_mouse_entered() -> void:
-	#current_control = controlTypes.Y
-	#print("In Y")
-#
-#
-#func _on_y_mouse_exited() -> void:
-	#current_control = controlTypes.OUTSIDE
-	#print("Out of Y")
+	pass
+
+func _on_y_mouse_entered() -> void:
+	current_control = gizmo_control_types.Y
+	print("In Y")
+
+
+func _on_y_mouse_exited() -> void:
+	current_control = gizmo_control_types.OUTSIDE
+	print("Out of Y")
