5c0d886f94fa9143082a2692bcca9316ad3e5f07
Felipe-Sena <lipesenacosta@gmail.com>
2024-05-12 22:36:45 -0300

    Changing scripts and materials around for an implementation that doesn't require a plugin

---
+++ Scenes/Gizmo/Shared/scripts/gizmo_control.gd
@@ -0,0 +1,32 @@
+class_name gizmo
+extends Node3D
+
+var move_gizmo: Node3D = null
+var rotate_gizmo: Node3D = null
+var scale_gizmo: Node3D = null
+var screen_manip: Node3D = null
+
+enum selected_gizmo {
+	MOVE,
+	ROTATE,
+	SCALE,
+	SCREEN_MANIP
+}
+
+func _init() -> void:
+	# Load all of the gizmo scene files in memory
+	move_gizmo = preload("res://Scenes/Gizmo/Move/moveGizmo.tscn").instantiate()
+	
+	# Assign the scenes into each gizmo variable
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+func add_gizmo_to_scene(gizmo_type: selected_gizmo, parent_node: Node3D) -> int:
+	match gizmo_type:
+		selected_gizmo.MOVE:
+			parent_node.add_child(move_gizmo)
+	
+	return OK
