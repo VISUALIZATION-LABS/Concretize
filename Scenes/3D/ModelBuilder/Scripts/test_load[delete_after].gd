5842b93ee87e1aa25d0913a782f06e58b8d5badb
Felipe-Sena <lipesenacosta@gmail.com>
2024-06-17 22:07:46 -0300

    C# testing

--- Scenes/3D/ModelBuilder/Scripts/test_load[delete_after].gd
+++ Scenes/3D/ModelBuilder/Scripts/test_load[delete_after].gd
@@ -1,6 +1,7 @@
 extends Node
 
 func _ready() -> void:
-	var compiled_mesh = ModelCompiler.compile_mesh()
-
-	self.add_child.call_deferred(compiled_mesh)
+	#var compiled_mesh = ModelCompiler.compile_mesh()
+	var mesh_compiler = preload("res://Global/ModelCompiler/model_compiler.cs").new()
+	#self.add_child.call_deferred(compiled_mesh)
+	self.add_child.call_deferred(mesh_compiler.compile_mesh("C:/Users/Felipe/dev/tcc/program/TestFiles/ModelsV2/Mesh_tests/Dense/StanfordDragon/stabfordDragonPureTri.obj"))
