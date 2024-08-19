4f11626ef9a1f19bcfc41a43a03b88ec26c7a858
Felipe-Sena <lipesenacosta@gmail.com>
2024-08-18 22:42:38 -0300

    Implement RAI

--- Scenes/2D/MainMenuUI/main_menu_ui.gd
+++ Scenes/2D/MainMenuUI/main_menu_ui.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@onready var model_compiler = preload("res://Global/ModelCompiler/model_compiler.cs")
+var model_importer: Node3D
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +9,17 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 
 func _on_button_button_down() -> void:
-	var compiled_mesh := model_compiler.new()
+	
+	#self.add_child.call_deferred(model_importer)
 
-	self.get_parent().get_parent().add_child.call_deferred(compiled_mesh.compile_obj_mesh("C:\\Users\\Felipe\\dev\\tcc\\program\\TestFiles\\ModelsV2\\Mesh_tests\\MaterialPyramid\\materialPyramid.obj"))
+	call_deferred("_compile")	
+
+func _compile() -> void:
+	model_importer = load("res://Scenes/3D/RuntimeAssetImporter/runtime_asset_importer.tscn").instantiate()
+	get_tree().current_scene.add_child(model_importer)
+	model_importer.Compile_mesh("/home/felipe/Development/Blender/Projects/Gallery_test/Gallery_test.obj")
