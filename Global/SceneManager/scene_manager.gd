a465300b00c438fce41cf426c24788a3c8bff2b2
Felipe-Sena <lipesenacosta@gmail.com>
2024-09-07 00:55:40 -0300

    Fix a longstanding bug where the scene manager wouldn't report everything that occurs on the tree. Add mesh importer

--- Global/SceneManager/scene_manager.gd
+++ Global/SceneManager/scene_manager.gd
@@ -11,24 +11,34 @@ func _enter_tree() -> void:
 	current_scene_tree.node_removed.connect(Callable(self, "_on_scene_node_removed"))
 
 func _on_scene_node_added(node: Node) -> void:
-	if node.get_parent() == current_scene_tree.root:
-		var path: String = node.name
-		if path in autoload:
-			print_rich("Loaded [b][i][color=yellow]%s[/color][/i][/b]." % path)
-		elif node.get_class() ==  "Control":
-			print_rich("Loaded [b][i][color=green]%s[/color][/i][/b]." % path)
-		elif node.get_class() == "Node3D":
-			print_rich("Loaded [b][i][color=purple]%s[/color][/i][/b]." % path)
-		else:
-			print_rich("Loaded [b][i][color=white]%s[/color][/i][/b]." % path)
+	var path: String = node.name
+	if path in autoload:
+		print_rich("Loaded [b][i][color=yellow]%s[/color][/i][/b]." % path)
+	elif node.get_class() ==  "Control":
+		print_rich("Loaded [b][i][color=green]%s[/color][/i][/b]." % path)
+	elif node.get_class() == "Node3D":
+		print_rich("Loaded [b][i][color=purple]%s[/color][/i][/b]." % path)
+	else:
+		print_rich("Loaded [b][i][color=white]%s[/color][/i][/b]." % path)
 
 func _on_scene_node_removed(node: Node) -> void:
-	if node.get_parent() == current_scene_tree.root:
-		var path: String = node.name
-		print_rich("[color=cyan]Unloaded [b][i]%s[/i][/b][/color]." % path)
+	var path: String = node.name
+	print_rich("[color=cyan]Unloaded [b][i]%s[/i][/b][/color]." % path)
 
 func change_current_scene_to_file(path: String) -> void:
 	current_scene_tree.change_scene_to_file(path)
 
 func change_current_scene_to_packed(packed_scene: PackedScene) -> void:
 	current_scene_tree.change_scene_to_packed(packed_scene)
+
+func import_mesh(path: PackedStringArray, 	) -> void:
+	if path.size() == 0:
+		ErrorManager.raise_error("Cannot import mesh (no file)")
+		return
+	
+	
+	var mesh_importer: Node3D = load("res://Scenes/3D/RuntimeAssetImporter/runtime_asset_importer.tscn").instantiate()
+
+	current_scene_tree.current_scene.add_child(mesh_importer)	
+
+	mesh_importer.Compile_mesh("some_place")
