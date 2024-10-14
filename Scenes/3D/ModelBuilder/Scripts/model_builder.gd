f8de73e6da47bd9d0ab391c7b0b13c9201147a44
Felipe-Sena <lipesenacosta@gmail.com>
2024-10-13 21:08:07 -0300

    Instead of creating a new static collider, just create the trimesh

--- Scenes/3D/ModelBuilder/Scripts/model_builder.gd
+++ Scenes/3D/ModelBuilder/Scripts/model_builder.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 @onready var model: MeshInstance3D = get_node("Mesh")
+@onready var collision_shape: CollisionShape3D = get_node("Mesh/StaticBody3D/CollisionShape3D")
 @onready var vertex_control: Node3D = load("res://Scenes/3D/Controls/MeshBuilder/VertexControl.tscn").instantiate()
+@onready var base_material: StandardMaterial3D = StandardMaterial3D.new()
 
 # Store mesh vertex positions
 var vertices: PackedVector3Array = [Vector3(-1, 0, -1), Vector3(1, 0, -1), Vector3(1, 0, 1), Vector3(-1, 0, 1)]
@@ -13,14 +15,15 @@ var vertex_controls: Array[Node3D] = []
 var st: SurfaceTool = SurfaceTool.new()
 
 func _ready() -> void:
+	base_material.albedo_color = Color("RED")
 	for vtx_pos in vertices:
 		var vtx_control: Node3D = vertex_control.duplicate()
 		vtx_control.position = vtx_pos
 		vertex_controls.append(vtx_control)
-		add_child(vtx_control)
+		add_child(vtx_control, true)
 
 	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	for vtx_idx in vertex_controls.size():
 		var vtx_control: Node3D = vertex_controls[vtx_idx]
 		vertices[vtx_idx] = vtx_control.position
@@ -29,10 +32,13 @@ func _process(delta: float) -> void:
 	st.begin(Mesh.PRIMITIVE_TRIANGLES)
 	st.set_smooth_group(-1)
 	st.add_triangle_fan(vertices)
+	st.set_material(base_material)
 	st.generate_normals()
 	#st.generate_tangents()
 	
 	model.mesh = st.commit()
+
+	collision_shape.shape = model.mesh.create_trimesh_shape()
 
 func _get_item(array: Array, index: int) -> Variant:
 	if index >= array.size():
