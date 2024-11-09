2e6c663aa2bc90f9d9b7d96b608720e1288ec9a1
Felipe-Sena <lipesenacosta@gmail.com>
2024-11-08 22:37:20 -0300

    Splash screen

---
+++ Scenes/2D/Splash/splash.gd
@@ -0,0 +1,11 @@
+extends Control
+
+# Does nothing yet tbh
+
+func _ready() -> void:
+	get_window().transparent = true
+	get_window().mode = Window.MODE_FULLSCREEN
+	
+	#await SceneManager.scene_tree.create_timer(3).timeout
+	
+	SceneManager.change_scene(load("res://Scenes/3D/MainScene/main_scene.tscn"))
