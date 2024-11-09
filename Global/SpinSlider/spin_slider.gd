109677e8f81ba9d717a4aec9801748f4a413e9bf
Felipe-Sena <lipesenacosta@gmail.com>
2024-11-08 22:36:55 -0300

    Implement the whole class

--- Global/SpinSlider/spin_slider.gd
+++ Global/SpinSlider/spin_slider.gd
@@ -1,23 +1,39 @@
 class_name SpinSlider
-extends Range
+extends MarginContainer
+
+signal value_changed(value: float)
+
+# Extending from the margin container is fucking stupid but fuck the ui
+# nodes in this engine fucking hell god fucking damn fuck this shit fuck you
+
+@onready var background_panel: PanelContainer = PanelContainer.new()
+@onready var stack: VBoxContainer = VBoxContainer.new()
+@onready var slider: HSlider = HSlider.new()
+@onready var input: LineEdit = LineEdit.new()
+
+var value: float = 0.0
+
+var step: float = 0.1
+
 
 func _ready() -> void:
-	# setup the nodes
+	slider.step = step
+	slider.value = value
+	slider.value_changed.connect(_slider_value_changed)
 	
-	self.custom_minimum_size = Vector2(128, 32)
+	background_panel.set_anchors_preset(Control.PRESET_FULL_RECT)
+	background_panel.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	
-	var debug_node: ReferenceRect = ReferenceRect.new()
-	debug_node.editor_only = false
+	input.set_anchors_preset(Control.PRESET_FULL_RECT)
+	input.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	input.text = str(self.value)
 	
-	var background: PanelContainer = PanelContainer.new()
-	var margin: MarginContainer = MarginContainer.new()
-	var line_input: LineEdit = LineEdit.new()
-	var slider: HSlider = HSlider.new()
+	self.add_child(background_panel)
+	background_panel.add_child(stack)
+	stack.add_child(input)
+	stack.add_child(slider)
+
+func _slider_value_changed(value: float) -> void:
+	input.text = str(value)
 	
-	line_input.text = str(self.value)
-	
-	self.add_child(background)
-	#self.add_child(debug_node.duplicate())
-	background.add_child(margin)
-	#background.add_child(debug_node.duplicate())
-	margin.add_child(line_input)
+	value_changed.emit(value)
