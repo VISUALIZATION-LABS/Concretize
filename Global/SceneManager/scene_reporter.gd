86647ecb7066d75e977db6c9996d931f6188bf98
Felipe-Sena <lipesenacosta@gmail.com>
2024-11-08 22:36:42 -0300

    \n

--- Global/SceneManager/scene_reporter.gd
+++ Global/SceneManager/scene_reporter.gd
@@ -5,24 +5,20 @@ extends Node
 # callable, and is part of scene management.
 
 enum PopupType {
-    BUTTON,
-    LOADING
+	BUTTON,
+	LOADING
 }
 
 func create_popup(title: String = "Title", description: String = "Description", type: PopupType = PopupType.BUTTON, primary_button_text: String = "Ok", secondary_button_text: String = "Cancel") -> Control:
-    var popup: Control = load("res://Scenes/2D/UI/Prefabs/Popup/internal_popup.tscn").instantiate()
+	var popup: Control = load("res://Scenes/2D/UI/Prefabs/Popup/internal_popup.tscn").instantiate()
 
-    popup.title = title
-    popup.description = description
-    popup.button_primary.text = primary_button_text
-    popup.button_secondary.text = secondary_button_text
-    popup.type = type
-
-
-    #SceneManager.scene_tree.current_scene.get_node("UI").add_child(popup)
-
-    return popup
+	popup.title = title
+	popup.description = description
+	popup.button_primary.text = primary_button_text
+	popup.button_secondary.text = secondary_button_text
+	popup.type = type
 
 
+	#SceneManager.scene_tree.current_scene.get_node("UI").add_child(popup)
 
-
+	return popup
