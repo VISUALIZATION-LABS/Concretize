07d6cb6f2fda2a35a8adf1c40d48efa858e11302
Felipe-Sena <lipesenacosta@gmail.com>
2024-11-13 06:50:24 -0300

    settings

--- Global/SettingsEntry/settings_entry.gd
+++ Global/SettingsEntry/settings_entry.gd
@@ -1,18 +1,32 @@
 class_name SettingsEntry
 extends Control
 
-@onready var split_container: HBoxContainer = HBoxContainer.new()
-@onready var label: Label = Label.new()
-@onready var options_button: OptionButton = OptionButton.new()
 
+signal item_selected(id: int)
+
+var _split_container: HBoxContainer = HBoxContainer.new()
+var _label: Label = Label.new()
+var _options_button: OptionButton = OptionButton.new()
+
+var title: String:
+	get:
+		return title
+	set(value):
+		title = value
+		_label.text = title
 
 func _ready() -> void:
-	label.text = "Lighting quality"
+	self.add_child(_split_container)
+	_split_container.custom_minimum_size = Vector2(228,0)
+	_split_container.add_child(_label)
+	_split_container.add_child(_options_button)
 	
-	self.add_child(split_container)
-	split_container.custom_minimum_size = Vector2(228,0)
-	split_container.add_child(label)
-	split_container.add_child(options_button)
+	_options_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	
-	options_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	
+	_options_button.item_selected.connect(_on_item_pressed)
+
+func add_item(label: String = "", id: int = -1) -> void:
+	_options_button.add_item(label, id)
+
+func _on_item_pressed(id: int) -> void:
+	item_selected.emit(id)
--- Scenes/2D/UI/Preferences/preferences.gd
+++ Scenes/2D/UI/Preferences/preferences.gd
@@ -1,14 +1,58 @@
 extends Control
 
+enum Settings {
+	# Lighting
+	EFFECTS_ULTRA,
+	EFFECTS_HIGH,
+	EFFECTS_MEDIUM,
+	EFFECTS_LOW,
+	
+	SHADOWS_ULTRA,
+	SHADOWS_HIGH,
+	SHADOWS_MEDIUM,
+	SHADOWS_LOW,
+	
+	SCALE_NATIVE,
+	SCALE_75,
+	SCALE_50,
+	SCALE_25
+	
+}
+
+@onready var entry_container: VBoxContainer = $PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var effects_quality: SettingsEntry = SettingsEntry.new()
+	effects_quality.item_selected.connect(_on_settings_item_selected)
 	
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
-	$PanelContainer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/VBoxContainer.add_child(SettingsEntry.new())
+	effects_quality.title = "Effects"
+	effects_quality.add_item("Ultra", Settings.EFFECTS_ULTRA)
+	effects_quality.add_item("High", Settings.EFFECTS_HIGH)
+	effects_quality.add_item("Medium", Settings.EFFECTS_MEDIUM)
+	effects_quality.add_item("Low", Settings.EFFECTS_LOW)
 	
+	var shadows_quality: SettingsEntry = SettingsEntry.new()
+	shadows_quality.item_selected.connect(_on_settings_item_selected)
+	
+	shadows_quality.title = "Shadows"
+	shadows_quality.add_item("Ultra", Settings.SHADOWS_ULTRA)
+	shadows_quality.add_item("High", Settings.SHADOWS_HIGH)
+	shadows_quality.add_item("Medium", Settings.SHADOWS_MEDIUM)
+	shadows_quality.add_item("Low", Settings.SHADOWS_LOW)
+	
+	var scale_quality: SettingsEntry = SettingsEntry.new()
+	scale_quality.item_selected.connect(_on_settings_item_selected)
+	
+	scale_quality.title = "Scaling"
+	scale_quality.add_item("Native", Settings.SCALE_NATIVE)
+	scale_quality.add_item("75%", Settings.SCALE_75)
+	scale_quality.add_item("50%", Settings.SCALE_50)
+	scale_quality.add_item("25%", Settings.SCALE_25)
+	
+	entry_container.add_child(effects_quality)
+	entry_container.add_child(shadows_quality)
+	entry_container.add_child(scale_quality)
+	
+func _on_settings_item_selected(id: int) -> void:
+	print(id)
